b7dd1279dfa74ab89636fbf6572c927007fb3601
jeremiahwood <jwood47@cnm.edu>
2016-11-10 13:15:51 -0700

    Still confused? but thought i finished it?

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-	<html lang ="eg">
+<html lang ="eg">
 	<head>
 		<meta charset="utf 8">
 		<meta http-equiv="x-ua-compatible"content="ie=edge">
@@ -7,7 +7,7 @@
 
 		<!--latest compiled and minified css-->
 		<div>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
 		</div>
 		<!-- Optional theme -->
 
